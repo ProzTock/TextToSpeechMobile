84d6a1afd4bf788213269c6aad474aa2970828b7
ProzTock <santiagopujana@gmail.com>
2022-03-06 21:35:53 -0500

    setting up text sender

--- Text_Sender/src/main.js
+++ Text_Sender/src/main.js
@@ -1,16 +1,54 @@
 const Realm = require("realm");
+const prompt = require('prompt-sync')({sigint: true});
 
-const REALM_APP_ID = "text_to_speech_mobile-cctth";
-login(REALM_APP_ID);
+main();
 
-async function login() {
+function main(){
 
-    const app = new Realm.App({ id: REALM_APP_ID });
-    const credentials = Realm.Credentials.anonymous();
+    const REALM_APP_ID = "text_to_speech_mobile-cctth";
+    const USERNAME = "Text Sender";
 
-    try {
+    login(REALM_APP_ID, USERNAME);
+}
+
+async function login(realm_app_id, username) {
+
+    const app = new Realm.App({ id: realm_app_id });
+    const credentials = Realm.Credentials.function({ username: username});
+
+    try{
         const user = await app.logIn(credentials);
-    } catch(err) {
+        typeMessage(user);
+    } catch(err){
         console.error("Failed to log in", err);
     }
-};
+}
+
+async function typeMessage(user_instance){
+
+    let message = "";
+
+    while(true){
+
+        console.log("\n--------------------------------\n")
+
+        message = prompt("What's your message?: -> ");
+
+        if(message !== "exit" && message !== "salir"){
+
+            console.log()
+
+            try{
+                const result = await user_instance.functions.sendMessage(message);
+                console.log(result);
+            }catch(err){
+                console.log(err.message);
+            }
+        }
+        else{
+            break;
+        }
+    }
+
+    process.exit(0);
+}
